97e2f84062da69d18946d230c6993685c34c83e1
vladryanka <vsmorzhok@mail.ru>
2025-03-25 23:52:13 +0300

    suspendCoroutine

--- app/src/main/java/com/smorzhok/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/smorzhok/factorialtest/MainViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.math.BigInteger
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
 
 class MainViewModel : ViewModel() {
 
@@ -18,11 +20,25 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
+
         viewModelScope.launch {
             val number = value.toLong()
+            val result = factorial(number)
+            _state.value = Factorial(result)
 
-            delay(1000)
-            _state.value = Factorial(factorial = number.toString())
         }
+    }
+
+    private suspend fun factorial(number: Long): String {
+        return suspendCoroutine {
+            thread {
+                var result = BigInteger.ONE
+                    for (i in 1..number) {
+                        result = result.multiply(BigInteger.valueOf(i))
+                    }
+                it.resumeWith(Result.success(result.toString()))
+            }
+        }
+
     }
 }
